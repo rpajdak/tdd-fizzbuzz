9a0936c29dc45cc08f621b2a3efefd9217cbf4a2
karolina <karolinabudzik92@gmail.com>
2020-09-06 14:16:57 +0200

    Third test passed.

--- src/main/java/com/codecool/FizzBuzz.java
+++ src/main/java/com/codecool/FizzBuzz.java
@@ -5,7 +5,11 @@ public class FizzBuzz {
     public String[] convert(int i) {
         String[] stringArray = new String[i];
         for (int j = 0; j < i; j++) {
-            stringArray[j] = String.valueOf(j + 1);
+            if ((j + 1) % 3 == 0) {
+                stringArray[j] = "Fizz";
+            } else {
+                stringArray[j] = String.valueOf(j + 1);
+            }
         }
         return stringArray;
     }
