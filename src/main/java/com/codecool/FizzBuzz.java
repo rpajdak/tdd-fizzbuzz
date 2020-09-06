07aba10d20dfe8ee5c2d889c3898bcdc0136a364
Rafal Pajdak <rpajdak@gmail.com>
2020-09-06 14:12:04 +0200

    clean code

--- src/main/java/com/codecool/FizzBuzz.java
+++ src/main/java/com/codecool/FizzBuzz.java
@@ -3,6 +3,10 @@ package com.codecool;
 public class FizzBuzz {
 
     public String[] convert(int i) {
-        return new String[]{"1"};
+        String[] stringArray = new String[i];
+        for (int j = 0; j < i; j++) {
+            stringArray[j] = String.valueOf(j + 1);
+        }
+        return stringArray;
     }
 }
