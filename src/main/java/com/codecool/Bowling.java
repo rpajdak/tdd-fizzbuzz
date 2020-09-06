dc0f16566017e131bc01cd4f0d1e7adbe605b775
Rafal Pajdak <rpajdak@gmail.com>
2020-09-06 15:19:43 +0200

    Third test Bowling passed.

--- src/main/java/com/codecool/Bowling.java
+++ src/main/java/com/codecool/Bowling.java
@@ -7,6 +7,9 @@ public class Bowling {
         for (int i = 0; i < 10; i++) {
             if (!frames[i][0].equals("X")) {
                 points += Integer.parseInt(frames[i][0]);
+                if (!frames[0][1].equals("-")) {
+                    points += Integer.parseInt(frames[0][1]);
+                }
             } else {
                 points += 30;
             }
