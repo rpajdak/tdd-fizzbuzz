1da1ba8115c1fc6e8f60d643d587e3b50ed30623
Rafal Pajdak <rpajdak@gmail.com>
2020-09-06 12:57:47 +0200

    First test failed

--- src/test/java/com/codecool/AppTest.java
+++ src/test/java/com/codecool/AppTest.java
@@ -1,20 +1,27 @@
 package com.codecool;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setup() {
+        fizzBuzz = new FizzBuzz();
     }
+
+
+    @Test
+    public void shouldReturnArrayWith1() {
+        String[] myStringArray = {"1"};
+        assertEquals(fizzBuzz.convert(1),myStringArray);
+    }
+
 }
